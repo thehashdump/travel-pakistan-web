1a170b9336d5af9b72d71de6e521c6215a0ee026
aitezazshah <aitezazshah123@gmail.com>
2023-02-04 23:42:06 +0500

    Add SignIn Page Asset

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import { Home } from '../home';
 import { TourList } from '../tourList';
+import { SignIn } from '../signIn';
 import Styles from './app.module.scss';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/tours" element={<TourList />} />
+					<Route path="/signin" element={<SignIn />} />
 				</Routes>
 			</Layout>
 		</div>
