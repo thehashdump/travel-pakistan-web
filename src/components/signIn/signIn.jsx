95a684234d374592134884421ea011b4fce1c8e5
aitezazshah <aitezazshah123@gmail.com>
2023-02-05 00:42:22 +0500

    Add form content to SignIn Page

--- src/components/signIn/signIn.jsx
+++ src/components/signIn/signIn.jsx
@@ -1,3 +1,4 @@
+import { Image } from 'react-bootstrap';
 import Styles from './signIn.module.scss';
 
 function SignIn() {
@@ -5,11 +6,56 @@ function SignIn() {
 		<div className={Styles.SignIn}>
 			<div className={Styles.image}>
 				<div>
-					<span className={Styles.heading}>Explore Pakistan With Us!</span><br/>
-					<span className={Styles.subtext}>Its time to unfold the beauty of Pakistan!</span>
+					<span className={Styles.heading}>Explore Pakistan With Us!</span>
+					<br />
+					<span className={Styles.subtext}>
+            Its time to unfold the beauty of Pakistan!
+					</span>
 				</div>
 			</div>
-			<div className={Styles.form}></div>
+			<div className={Styles.form}>
+				<div className={Styles.formcontent}>
+					<div>
+						<span className={Styles.signinheading}>SignIn</span>
+						<form>
+							<input placeholder="Email Address" className={Styles.email} required/>
+							<input
+								type="password"
+								placeholder="Password"
+								className={Styles.password}
+								required
+							/>
+							<button className={Styles.loginbtn}>Login</button>
+							<div className={Styles.noaccount}>
+								<span>
+                Not have a account?
+									<span className={Styles.redirectlink}>SignUp</span>
+								</span>
+							</div>
+							<div className={Styles.borderline}>
+								<hr/>
+								<span>Or</span>
+								<hr/>
+							</div>
+							<div className={Styles.sociallogin}>
+								<span>Login Using</span>
+								<Image
+									src={require('../../assets/google.png')}
+									alt="Google"
+								/>
+								<Image
+									src={require('../../assets/facebook.png')}
+									alt="Facebook"
+								/>
+								<Image
+									src={require('../../assets/twitter.png')}
+									alt="Twitter"
+								/>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
