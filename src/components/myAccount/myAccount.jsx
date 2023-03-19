41da9742f5a9fb4ac5a12805d6ccaa41f331bdab
aitezazshah <aitezazshah123@gmail.com>
2023-03-19 14:00:30 +0500

    Create Top section in myAccount component

--- src/components/myAccount/myAccount.jsx
+++ src/components/myAccount/myAccount.jsx
@@ -1,10 +1,18 @@
-// import Styles from "./myAccount.module.scss";
+import Styles from "./myAccount.module.scss";
+import { Navbar } from "../navbar";
+import { Footer } from "../footer";
 
 function MyAccount() {
 	return (
-		<div>
-        Hello
-		</div>
+		<div className={Styles.myAccount}>
+			<div className={Styles.top}>
+				<Navbar />
+				<div className={Styles.divTop}>
+					<span>Welcome, Syed Aitezaz Shah</span>
+				</div>
+			</div>
+            <Footer/>
+        </div>    
 	);
 }
 
