2f304c0a645701f34f93e3c0e4c3e22d5cb64d50
aitezazshah <aitezazshah123@gmail.com>
2023-03-19 23:06:53 +0500

    Add left side bar to the component

--- src/components/myAccount/myAccount.jsx
+++ src/components/myAccount/myAccount.jsx
@@ -1,8 +1,14 @@
+import { useState } from 'react';
 import Styles from './myAccount.module.scss';
 import { Navbar } from '../navbar';
 import { Footer } from '../footer';
 
 function MyAccount() {
+	const [activeTab, setActiveTab] = useState('Activity Feed');
+
+	const handleTabClick = (tab) => {
+		setActiveTab(tab);
+	};
 	return (
 		<div className={Styles.myAccount}>
 			<div className={Styles.top}>
@@ -20,18 +26,11 @@ function MyAccount() {
 						<div className={Styles.accountInfo}>
 							<div className={Styles.details}>
 								<div className={Styles.name}>
-									<span>
-                                         Syed Aitezaz Imtiaz
-									</span>
-
+									<span>Syed Aitezaz Imtiaz</span>
 								</div>
 								<div className={Styles.button}>
-									<button className={Styles.edit}>
-                                        Edit Profile
-									</button>
-
+									<button className={Styles.edit}>Edit Profile</button>
 								</div>
-
 							</div>
 							<div className={Styles.stats}>
 								<div className={Styles.booking}>
@@ -44,12 +43,42 @@ function MyAccount() {
 								</div>
 							</div>
 						</div>
-
 					</div>
 				</div>
 
+				<div className={Styles.container}>
+					<div className={Styles.leftSidebar}>
+						<ul>
+							<li
+								className={activeTab === 'Activity Feed' ? Styles.active : ''}
+								onClick={() => handleTabClick('Activity Feed')}
+							>
+                                Activity Feed
+							</li>
+							<li
+								className={activeTab === 'My Bookings' ? Styles.active : ''}
+								onClick={() => handleTabClick('My Bookings')}
+							>
+                                My Bookings
+							</li>
+							<li
+								className={activeTab === 'Private Tour Requests' ? Styles.active : ''}
+								onClick={() => handleTabClick('Private Tour Requests')}
+							>
+                                Private Tour Requests
+							</li>
+							<li
+								className={activeTab === 'History' ? Styles.active : ''}
+								onClick={() => handleTabClick('History')}
+							>
+                                History
+							</li>
+						</ul>
+					</div>
+
+				</div>
 			</div>
-			<Footer/>
+			<Footer />
 		</div>
 	);
 }
