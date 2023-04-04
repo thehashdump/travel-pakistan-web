8861029ba2daf4bbc6235808fc67ec1e12b0e2eb
aitezazshah <aitezazshah123@gmail.com>
2023-04-04 05:33:01 +0500

    Create Scaffolding for private-tour-request

---
+++ src/components/privateTour/privateTour.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import Styles from './privateTour.module.scss';
+
+function PrivateTour() {
+	return (
+		<div className={Styles.private}>
+        Hello
+		</div>
+	);
+}
+
+export { PrivateTour };
