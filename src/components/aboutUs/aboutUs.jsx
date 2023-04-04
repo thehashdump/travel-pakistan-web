654ba6868c09c5e8db01144966a550076c2f8f80
aitezazshah <aitezazshah123@gmail.com>
2023-04-04 09:01:21 +0500

    Create Scaffolding for about-us page

---
+++ src/components/aboutUs/aboutUs.jsx
@@ -0,0 +1,20 @@
+import { Navbar } from '../navbar';
+import { Footer } from '../footer';
+import Styles from './aboutUs.module.scss';
+
+function AboutUs() {
+	return (
+		<div className={Styles.aboutUs}>
+			<div className={Styles.top}>
+				<Navbar />
+				<div className={Styles.divTop}>
+          About Us
+				</div>
+			</div>
+
+			<Footer />
+		</div>
+	);
+}
+
+export { AboutUs };
