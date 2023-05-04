6ecbc0f2738eef657481a503ed3d42df530015d7
aitezazshah <aitezazshah123@gmail.com>
2023-05-04 23:18:59 +0500

    create scaffolding for private-tour-responses

---
+++ src/components/myAccount/privateResponse/privateResponse.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import Styles from './privateResponse.module.scss';
+
+function PrivateResponse() {
+	return (
+
+		<div className={Styles.hello}>hello</div>
+	);
+}
+
+export { PrivateResponse };
